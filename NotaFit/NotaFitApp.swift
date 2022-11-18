234757a4489c07e0143e7afa0b85c4f629a8f866
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-11-18 13:54:24 -0800

    Initial Commit

---
+++ NotaFit/NotaFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotaFitApp.swift
+//  NotaFit
+//
+//  Created by Alyssa Tadeo on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NotaFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
